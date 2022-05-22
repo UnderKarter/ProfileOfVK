7f6d3039c2fd9eac6eb5d24131be69642eb2a678
Semenov Petr <under_karter49@icloud.com>
2022-05-23 00:30:56 +0300

    Initial Commit

---
+++ ProfileOfVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileOfVK
+//
+//  Created by temp user on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
